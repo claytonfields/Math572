717b11191e2dbb61f1dc8dfb55cdcdccd1562df6
Clayton <claytonfields@u.boisestate.edu>
2020-02-17 17:41:01 -0700

    Got the second problem on HW 5 working

--- Fields_HW05.py
+++ Fields_HW05.py
@@ -9,6 +9,7 @@ Created on Sat Feb 15 13:06:44 2020
 import numpy as np
 import pandas as pd
 from scipy.linalg import inv
+from numpy import exp
 
 
 def N_t(N_0,K,r,t):
@@ -48,15 +49,38 @@ def LSE(N,N_0,K,r,t):
     return ((N-N_t(N_0,K,r,t))**2).sum()
 
 def dLdr(N,N_0,K,r,t):
-    term1 = (-2*N*dNdr(N_0,K,r,t)).sum()
-    term2 = ((2*K**2*N_0**2*t*(K-N_0)*np.exp(-r*t))/((K-N_0)*np.exp(-r*t)+N_0)**3).sum()
-    return term1 + term2
+    term1 = ((-2*K*N_0*t*N*(K-N_0)*exp(-r*t))/(N_0+(K-N_0)*exp(-r*t))**2)
+    term2 = ((2*K**2*N_0**2*t*(K-N_0)*np.exp(-r*t))/((K-N_0)*np.exp(-r*t)+N_0)**3)
+    return term1.sum() + term2.sum()
 
 def dLdK(N,N_0,K,r,t):
-    term1 = -2*N*dNdK(N_0,K,r,t).sum()
+#    term1 = -2*N*dNdK(N_0,K,r,t).sum()
+    term1 = (-2*N_0**2*N*exp(r*t)*(exp(r*t)-1))/(K+N_0*(exp(r*t)-1))**2
+    term2 = (2*K*N_0**3*exp(2*r*t)*(exp(r*t)-1))/(K+N_0*(exp(r*t)-1))**3
+    return term1.sum() + term2.sum()
+
+def L_rr(N,N_0,K,r,t):
+    term1 = (2*K*N_0*t**2*N*(K-N_0)*exp(r*t)*(-K+N_0*exp(r*t)+N_0))/(K+N_0*(exp(r*t)-1))**3
+    term2 = (2*K**2*N_0**2*t**2*(K-N_0)*exp(2*r*t)*(2*K-N_0*exp(r*t)-2*N_0))/(K+N_0*exp(r*t)-N_0)**4
+    return term1.sum() + term2.sum()
+
+def L_kk(N,N_0,K,r,t):
+    term1 = (4*N_0**2*N*exp(r*t)*(exp(r*t)-1))/(K+N_0*(exp(r*t)-1))**3
+    term2 = (2*N_0**3*exp(2*r*t)*(exp(r*t)-1)*(-2*K+N_0*exp(r*t)-N_0))/(K+N_0*exp(r*t)-N_0)**4
+    return term1.sum() + term2.sum()
     
+#def L_rk(N,N_0,K,r,t):
+#    term1 = (4*N_0**2*N*exp(r*t)*(exp(r*t)-1))/(K+N_0*(exp(r*t)-1))**3
+#    term2 = (2*N_0**3*exp(2*r*t)*(exp(r*t)-1)*(-2*K+N_0*exp(r*t)-N_0))/(K+N_0*exp(r*t)-N_0)**4
+#    return term1.sum() + term2.sum()
+
+def L_rk(N,N_0,K,r,t):
+    term1 = (-2*N_0**2*t*N*exp(r*t)*(2*K*exp(r*t)-K-N_0*exp(r*t)+N_0))/(K+N_0*(exp(r*t)-1))**3
+    term2 = (2*K*N_0**3*t*exp(2*r*t)*(3*K*exp(r*t)-2*K-2*N_0*exp(r*t)+2*N_0))/(K+N_0*exp(r*t)-N_0)**4
+    return term1.sum() + term2.sum()
 
 #def Lgrad(N,N_0,K,r,t):
+    
 #    return np.gradient(LSE(N,N_0,K,r,t)).sum()
 #
 
@@ -81,14 +105,36 @@ x = N-N_t(N_0,K,r,t)
 maxiter = 12
 
 #Implement Gauss Newton Method
+print('Gauss-Newton Method')
 for i in range(maxiter):
     print("r: %.3f, K: %d" %(theta[0],np.round(theta[1])))
     theta = theta+inv(A.T@A)@(A.T@x)
     A = np.hstack((dNdr(N_0,theta[1],theta[0],t),dNdK(N_0,theta[1],theta[0],t)))
     x = N-N_t(N_0,theta[1],theta[0],t)
     
+t = data['days'].to_numpy()
+N = data['beetles'].to_numpy()
 
 
+N_0=N[0]
+K = 1200
+r = .17
+theta = [r,K]
+maxiter = 20
+
+grad = np.array([dLdr(N,N_0,K,r,t),dLdK(N,N_0,K,r,t)])
+H = np.array([[L_rr(N,N_0,K,r,t),L_rk(N,N_0,K,r,t)],[L_rk(N,N_0,K,r,t),L_kk(N,N_0,K,r,t)]])
+
+    
+#Implement Newton Raphson
+print()
+print('Newton-Raphson Method')
+for i in range(maxiter):
+    print("r: %.3f, K: %d" %(theta[0],np.round(theta[1])))
+    theta = theta-inv(H)@grad
+    grad = np.array([dLdr(N,N_0,theta[1],theta[0],t),dLdK(N,N_0,theta[1],theta[0],t)])
+    H = np.array([[L_rr(N,N_0,theta[1],theta[0],t),L_rk(N,N_0,theta[1],theta[0],t)],\
+                   [L_rk(N,N_0,theta[1],theta[0],t),L_kk(N,N_0,theta[1],theta[0],t)]])
 
 
 
